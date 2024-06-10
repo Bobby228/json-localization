920b5cc63fe8a078c754ca1b415256662bef879e
Максим Бобров <bobrovmx@gmail.com>
2024-06-10 17:45:13 +0300

    Added Language models

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,6 +26,8 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'account_type' => fake()->randomElement(['ltd', 'freelancer', 'individual']),
+            'company_name' => fake()->company(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
